9f7d56353d0866629a28ab5af61cfafb163a409e
Xenz <ravensvenix@gmail.com>
2023-07-07 01:27:18 +0800

    Py-AutoClicker

--- lib/checkPassword.py
+++ lib/checkPassword.py
@@ -2,7 +2,7 @@ import pyautogui as py
 from lib.click import click
 
 def check_password(password):
-    if password == "PianoTiles":
+    if password == "Py-AutoClick":
         print("Password Correct!")
     else:
         print("Password Incorrect!")
